c5c8ced6c526713b8f238fc6289928e14a724deb
Tales Sousa Lins <64812534+taleslins@users.noreply.github.com>
2025-04-12 13:03:40 -0300

    Add files via upload

---
+++ PTI-Java/src/cargreenlp/Produto.java
@@ -0,0 +1,10 @@
+package cargreenlp;
+public class Produto {
+    String nome;
+    int quantidade;
+
+    public Produto(String nome, int quantidade) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+    }
+}
